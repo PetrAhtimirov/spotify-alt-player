1144a27c5fe75535bbc089756af4083833bae2f3
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2025-02-22 10:05:59 +0300

    deploy workflow

--- src/components/PlaybackRange.tsx
+++ src/components/PlaybackRange.tsx
@@ -10,7 +10,7 @@ const PlaybackRange = () => {
 
   const formatTime = (ms: number) => {
     const minutes = Math.floor(ms / 60000);
-    const seconds = Math.floor((ms % 60000) / 1000).toString().padStart(2, '0'); // Добавляем ведущий ноль
+    const seconds = Math.floor((ms % 60000) / 1000).toString().padStart(2, '0');
     return `${minutes}:${seconds}`;
   };
 
